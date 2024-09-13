1d596a8f380e2cec23d47044b55ab9dabc78760e
petyasoft <168456597+petyasoft@users.noreply.github.com>
2024-09-13 18:25:57 +0300

    Update telegram.py

--- utils/telegram.py
+++ utils/telegram.py
@@ -1,67 +1,83 @@
-from loguru import logger
+from utils.core import logger
+from pyrogram import Client
 from data import config
-import pyrogram
-from data.config import USE_PROXY
-import random
+import os
 
-async def create_sessions():
-    while True:
-        session_name = input('Введите название сессии (для выхода нажмите Enter)\n')
-        if not session_name:
-            return
-        
-        if USE_PROXY:
+class Accounts:
+    def __init__(self):
+        self.workdir = config.WORKDIR
+        self.api_id = config.API_ID
+        self.api_hash = config.API_HASH
+
+    def pars_sessions(self):
+        sessions = []
+        for file in os.listdir(self.workdir):
+            if file.endswith(".session"):
+                sessions.append(file.replace(".session", ""))
+
+        logger.info(f"Найдено сессий: {len(sessions)}!")
+        return sessions
+
+    async def check_valid_sessions(self, sessions: list):
+        logger.info(f"Проверяю сессии на валидность!")
+        valid_sessions = []
+        if config.USE_PROXY:
             proxy_dict = {}
             with open('proxy.txt','r') as file:
                 proxy_list = [i.strip().split() for i in file.readlines() if len(i.strip().split()) == 2]
                 for prox,name in proxy_list:
                     proxy_dict[name] = prox
-            
-            if session_name in proxy_dict:
-                proxy = proxy_dict[session_name]
-                proxy_client = {
-                    "scheme": config.PROXY_TYPE,
-                    "hostname": proxy.split(':')[0],
-                    "port": int(proxy.split(':')[1]),
-                    "username": proxy.split(':')[2],
-                    "password": proxy.split(':')[3],
-                }
+            for session in sessions:
+                try:
+                    if session in proxy_dict:
+                        proxy = proxy_dict[session]
+                        proxy_client = {
+                            "scheme": config.PROXY_TYPE,
+                            "hostname": proxy.split(':')[0],
+                            "port": int(proxy.split(':')[1]),
+                            "username": proxy.split(':')[2],
+                            "password": proxy.split(':')[3],
+                        }
+                        client = Client(name=session, api_id=self.api_id, api_hash=self.api_hash, workdir=self.workdir,proxy=proxy_client)
+
+                        if await client.connect():
+                            valid_sessions.append(session)
+                        else:
+                            logger.error(f"{session}.session is invalid")
+
+                        await client.disconnect()
+                    else:
+                        client = Client(name=session, api_id=self.api_id, api_hash=self.api_hash, workdir=self.workdir)
+
+                        if await client.connect():
+                            valid_sessions.append(session)
+                        else:
+                            logger.error(f"{session}.session is invalid")
+                        await client.disconnect()       
+                except:
+                    logger.error(f"{session}.session is invalid")
+            logger.success(f"Валидных сессий: {len(valid_sessions)}; Невалидных: {len(sessions)-len(valid_sessions)}")
                 
-                session = pyrogram.Client(
-                    api_id=config.API_ID,
-                    api_hash=config.API_HASH,
-                    name=session_name,
-                    workdir=config.WORKDIR,
-                    proxy=proxy_client
-                )
-
-                async with session:
-                    user_data = await session.get_me()
-
-                logger.success(f'Добавлена сессия +{user_data.phone_number} @{user_data.username} PROXY {proxy.split(":")[0]}')
-            else:
-                
-                session = pyrogram.Client(
-                    api_id=config.API_ID,
-                    api_hash=config.API_HASH,
-                    name=session_name,
-                    workdir=config.WORKDIR
-                )
-
-                async with session:
-                    user_data = await session.get_me()
-
-                logger.success(f'Добавлена сессия +{user_data.phone_number} @{user_data.username} PROXY : NONE')
         else:
-            
-            session = pyrogram.Client(
-                api_id=config.API_ID,
-                api_hash=config.API_HASH,
-                name=session_name,
-                workdir=config.WORKDIR
-            )
+            for session in sessions:
+                try:
+                    client = Client(name=session, api_id=self.api_id, api_hash=self.api_hash, workdir=self.workdir)
 
-            async with session:
-                user_data = await session.get_me()
+                    if await client.connect():
+                        valid_sessions.append(session)
+                    else:
+                        logger.error(f"{session}.session is invalid")
+                    await client.disconnect()
+                except:
+                    logger.error(f"{session}.session is invalid")
+            logger.success(f"Валидных сессий: {len(valid_sessions)}; Невалидных: {len(sessions)-len(valid_sessions)}")
+        return valid_sessions
 
-            logger.success(f'Добавлена сессия +{user_data.phone_number} @{user_data.username} PROXY : NONE')
+    async def get_accounts(self):
+        sessions = self.pars_sessions()
+        accounts = await self.check_valid_sessions(sessions)
+
+        if not accounts:
+            raise ValueError("Нет валидных сессий")
+        else:
+            return accounts
