3bdeba4d4396b8a0d699667e4b2c7a4946810c1a
petyasoft <168456597+petyasoft@users.noreply.github.com>
2024-09-13 18:25:24 +0300

    Create config.py

---
+++ data/config.py
@@ -0,0 +1,50 @@
+# api id, hash
+API_ID = 11111111
+API_HASH = 'Your api hash'
+
+USE_TG_BOT = False # True if you want use tg, else False
+BOT_TOKEN = '' # API TOKEN get in @BotFather
+CHAT_ID = '' # Your telegram id
+
+# задержка между подключениями к аккаунтам
+ACC_DELAY = [5, 150]
+
+# тип прокси
+PROXY_TYPE = "socks5" # http/socks5
+
+# папка с сессиями (не менять)
+WORKDIR = "sessions/"
+
+# использование прокси
+USE_PROXY = True # True/False
+
+# реф код, идет после startapp=
+REF_CODE = '707649803'
+
+# задержка между тасками
+TASK_SLEEP = [30,60] #[min,max]
+
+# задержка между минииграми
+GAME_SLEEP = [30,120] #[min,max]
+
+# айди тасков которые проверяются
+BLACKLIST_TASKS = {33,21,27,20,36,80,77}
+
+# играть в данную мини игру True/False
+HOLD_COIN = True
+ROULETTE = True
+SWIPE_COIN = True
+
+# мини задержки
+MINI_SLEEP = [5,15] #[min,max]
+
+BIG_SLEEP = [5*60*60,8*60*60]
+
+hello ='''              _                               __  _        
+ _ __    ___ | |_  _   _   __ _  ___   ___   / _|| |_  ___ 
+| '_ \  / _ \| __|| | | | / _` |/ __| / _ \ | |_ | __|/ __|
+| |_) ||  __/| |_ | |_| || (_| |\__ \| (_) ||  _|| |_ \__ \\
+| .__/  \___| \__| \__, | \__,_||___/ \___/ |_|   \__||___/
+|_|                |___/        
+
+'''
